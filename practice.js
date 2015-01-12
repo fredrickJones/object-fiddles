1646666fdab01ef7a47db20d5af0e37eac32ad2d
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-01-12 12:34:57 -0700

    update

--- practice.js
+++ practice.js
@@ -4,35 +4,42 @@
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
   //Code here
+var me = {
+  name: 'Fredrick',
+  age: 31
+};
 
 
 
 
 //NEXT PROBLEM
-
-
-
 
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
   //Code here
-
+var favoriteThings = {
+  band: 'U2',
+  food: 'hamburger',
+  person: 'Kirsten',
+  book: 'Harry Potter',
+  movie: 'Hobbit',
+  holiday: 'Christmas'
+};
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
   //Code here
-
+favoriteThings.car = 'subaru';
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
 
   //Code here
-
+favoriteThings.food = 'Lettuce';
+favoriteThings.book = '50 Shades of Gray';
 
 
 
 //NEXT PROBLEM
-
-
 
 /*Create an empty Object called backPack. Now, create a variable called item and
 set it equal to the string 'firstPocket'. Now, using bracket notation, add a 'firstPocket'
@@ -41,73 +48,100 @@ add a key (or property) to your backPack object that is named color,
 with the value being the color of your backpack. */
 
   //Code here
+var backPack = {};
+var item = 'firstPocket';
+backPack['item'] = 'firstPocket';
+backPack.color = 'Black';
 
 //After you do the above, alert your entire backPack object.
 
   //Code here
+alert(backPack);
 
 /*You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. */
 
   //Code here
-
+console.log(backPack);
+Object {
+  item: "firstPocket",
+  color: "Black"
+};
 
 
 
 //NEXT PROBLEM
-
-
-
 
 //Create another 'me' object with the following properties name, age, height, gender, married, eyeColor, hairColor. Fill those properties in with the appropriate values.
 
   //Code Here
+var me = {
+  name: 'Fredrick',
+  age: 31,
+  height: 6.1,
+  gender: 'Male',
+  married: true,
+  eyeColor: 'green',
+  hairColor: 'red'
+};
 
 //Now, loop through your object and alert every value. *Tyler --> 24 --> 6'0 --> Male, etc etc
 
   //Code Here
-
+for(var key in me) {
+  alert(me[key]);
+};
 
 
 
 //NEXT PROBLEM
-
-
-
 
 //Create an Object called 'album' with 5 keys named different song titles that you make up, with the values being the length of each song.
 
   //Code Here
+var album = {
+  jamJam: 234,
+  hamSlam: 342,
+  blamBam: 249,
+  gamHam: 453,
+  samSam: 652
+};
 
 //Now, loop through your album object alerting every song title individually.
 
   //Code Here
-
+for(var key in album) {
+  alert(key);
+};
 
 
 
 //NEXT PROBLEM
-
-
-
 
 //Create an object called states that has 5 US states as properties with the values being their population (doesn't have to be accurate).
 
   //Code Here
+var states = {
+  texas: 239023093,
+  california: 984792304982,
+  utah: 10993,
+  main: 9824309,
+  pennsylvania: 98202090
+};
 
 //Now, loop through your states object and if the states population is greater than 30K, alert that state.
 
   //Code Here
-
+for(var key in states) {
+  if(states[key] > 30000) {
+    alert(key);
+  };
+};
 
 
 
 //NEXT PROBLEM
-
-
-
-
 var user = {
     name: 'Tyler McGinnis',
     email: null,
@@ -115,23 +149,31 @@ var user = {
     birthday: undefined,
     username: 'tylermcginnis33',
     age: 0
-}
+};
+
 /*Above you're given a user object. Loop through the user object checking to make sure
 that each value is truthy. If it's not truthy, remove it from the object. */
 
   //Code Here
+for(var key in user) {
+  if(user[key]) {
+    alert(key);
+  }
+  else {
+    delete user[key];
+  };
+};
 
 //Once you get your truthy Object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
   //Code Here
-
+user.name = 'Fredrick Jones';
+user.pwHash = 'U+lsdlijf3oids';
+user.username = 'fredrickJones';
 
 
 
 //NEXT PROBLEM
-
-
-
 
 var user = {
         name: 'Tyler McGinnis',
